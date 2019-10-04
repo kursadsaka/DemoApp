cc4d9dbbdeb18f66eb78ce85033e5ccab665146b
kursadsaka <ksaka@etu.edu.tr>
2019-10-05 00:23:48 +0300

    Fonksiyon olusturuldu ve test etmek icin 3 tane unit test olusturuldu

--- myDemoApp/src/main/java/com/mycompany/app/App.java
+++ myDemoApp/src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,37 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.ArrayList;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
     }
+    
+    public static boolean searchAndSum(ArrayList<Integer> array, int e, ArrayList<Integer> array2, int e2) {
+
+        System.out.println("inside searchAndSum");
+
+        boolean islem1 = false;
+        boolean islem2 = false;
+        int sum = 0;
+
+        if (array != null)
+            for (int elt : array)
+                if (elt == e * e * e)
+                    islem1 = true;
+
+        if (array2 != null)
+            for (int elt : array2)
+                sum += elt * e;
+
+        if (sum == e2)
+            islem2 = true;
+
+        return islem1 && islem2;
+
+    }
+
 }
--- myDemoApp/src/test/java/com/mycompany/app/AppTest.java
+++ myDemoApp/src/test/java/com/mycompany/app/AppTest.java
@@ -3,21 +3,20 @@ package com.mycompany.app;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest (String testName) {
+        super(testName);
     }
 
     /**
@@ -35,4 +34,23 @@ public class AppTest
     {
         assertTrue( true );
     }
+    
+    public void testFoundTheCubeAndSum() {
+		ArrayList<Integer> array = new ArrayList<>(Arrays.asList(11, 112, 36, 4, 64));
+		ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(3, 4, 5));
+		assertTrue(new App().searchAndSum(array, 4, array2, 48));
+	}
+    
+    public void testNotFoundTheCube() {
+		ArrayList<Integer> array = new ArrayList<>(Arrays.asList(11, 112, 36, 4));
+		ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(3, 4, 5));
+		assertFalse(new App().searchAndSum(array, 4, array2, 48));
+	}
+    
+    public void testCouldNotGetTheSum() {
+		ArrayList<Integer> array = new ArrayList<>(Arrays.asList(11, 112, 36, 4, 64));
+		ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(3, 4, 5));
+		assertFalse(new App().searchAndSum(array, 4, array2, 49));
+	}
+    
 }
